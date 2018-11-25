60d0d6e640ac61566f50324d4af6577059863e7e
CLooker <chad.looker@gmail.com>
2018-11-25 02:32:32 -0500

    remove code because code that remains is nonetheless still explanatory

--- exercises/1.27/1.27.spec.js
+++ exercises/1.27/1.27.spec.js
@@ -4,16 +4,13 @@ const smallestDivisor = require('../1.23/1.23');
 const isPrime = num => num === smallestDivisor(num);
 
 describe('$fermatTest relationship with prime and Carmichael numbers', () => {
-  const CarmichaelNums = [561, 1105, 1729, 2465, 2821, 6601];
-  const primes = getPrimes(0, 1000);
-
-  primes.forEach(prime => {
+  getPrimes(0, 1000).forEach(prime => {
     test('$fermatTest is not tricked', () => {
       expect(fermatTest(prime)).toBeTruthy();
     });
   });
 
-  CarmichaelNums.forEach(CarmichaelNum => {
+  [561, 1105, 1729, 2465, 2821, 6601].forEach(CarmichaelNum => {
     test('$fermatTest is tricked', () => {
       expect(fermatTest(CarmichaelNum)).not.toBe(isPrime(CarmichaelNum));
     });
