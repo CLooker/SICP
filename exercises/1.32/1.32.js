1b5e1c7ebbf97f935775a3fb0bec643871d3cc1d
CLooker <chad.looker@gmail.com>
2019-05-27 17:46:07 -0400

    Fix bug so recursion can be used in accumulate fn

--- exercises/1.32/1.32.js
+++ exercises/1.32/1.32.js
@@ -9,16 +9,14 @@ const add = (a, b) => a + b;
 const inc = num => ++num;
 const identity = x => x;
 
-// this captures SICP spirit better
-// but v8 doesn't care about TCO
-// so this causes stack overflow
-const accumulateSICP = (updateTotalFn, updateMinFn, updateTermFn) => (
-  min,
-  max,
+const accumulate = (
+  updateTotalFn,
+  updateMinFn,
+  updateTermFn,
   startingTotal
-) => {
+) => (min, max) => {
   const fastAccumulate = (currMin, total) =>
-    min > max
+    currMin > max
       ? total
       : fastAccumulate(
           updateMinFn(currMin),
@@ -26,19 +24,6 @@ const accumulateSICP = (updateTotalFn, updateMinFn, updateTermFn) => (
         );
 
   return fastAccumulate(min, startingTotal);
-};
-
-// unsexy but gets it done
-const accumulate = (
-  updateTotalFn,
-  updateMinFn,
-  updateTermFn,
-  startingTotal
-) => (min, max) => {
-  let total = startingTotal;
-  for (let currMin = min; currMin <= max; currMin = updateMinFn(currMin))
-    total = updateTotalFn(total, updateTermFn(currMin));
-  return total;
 };
 
 const product = accumulate(multiply, inc, identity, 1);
